00c05927456f9fddc9ea9b83b11bad7e70533d85
Evgeniy Fomenko <49038282+EvgeniyFomenko@users.noreply.github.com>
2025-11-12 10:35:04 +0500

    feature добавил обработчик исключений

---
+++ src/main/java/ru/yandex/practicum/exception/ExceptionController.java
@@ -0,0 +1,22 @@
+package ru.yandex.practicum.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@RestControllerAdvice
+public class ExceptionController {
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ApiError wrongPostEvent(NotFoundException notFoundException) {
+        return ApiError.builder().message(notFoundException.getMessage())
+                .timestamp(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")))
+                .status("NOT_FOUND")
+                .reason("Incorrectly made request.")
+                .build();
+    }
+}
