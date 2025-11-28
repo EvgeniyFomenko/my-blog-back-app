244868c4adf107827d32527c73a8865df3e4721d
Evgeniy Fomenko <49038282+EvgeniyFomenko@users.noreply.github.com>
2025-11-29 02:11:44 +0500

    feature переписал классы моделей под спринг бут анатации круд репозиториев

--- src/main/java/ru/yandex/practicum/model/Image.java
+++ src/main/java/ru/yandex/practicum/model/Image.java
@@ -1,12 +1,23 @@
 package ru.yandex.practicum.model;
 
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
 
+@Table(name = "IMAGE")
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Image {
+    @Id
+    @Column
     private Long id;
+    @Column
     private String name;
+    @Column
     private Long postId;
 }
--- src/main/java/ru/yandex/practicum/model/Post.java
+++ src/main/java/ru/yandex/practicum/model/Post.java
@@ -4,16 +4,27 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
 
+@Table(name = "POST")
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class Post {
+    @Id
+    @Column
     private Long id;
+    @Column
     private String title;
+    @Column
     private String text;
+    @Column
     private String tags;
+    @Column
     private int likesCount;
+    @Column
     private int commentsCount;
 }
